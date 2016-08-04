90800a20ba756ec775e1a36c9143df2b05b18860
nayeem syed <developerinlondon@gmail.com>
2016-08-04 09:51:25 +0100

    updated version

--- 000-init.sh
+++ 000-init.sh
@@ -28,12 +28,28 @@ bashdlist() {
 }
 
 install_core() {
+  # install rvm
+  \curl -sSL https://get.rvm.io | bash
+  rvm install 2.1.2
+
+  # install cpanmin.us
+  \curl -L http://cpanmin.us  | perl - App::cpanminus --sudo
+
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   echo export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH" >> ~/.bashrc
+
+  # install brew cask
+  brew tap caskroom/cask
+  brew install brew-cask
+
+  brew install autojump
+
+  brew install htop
+
+  # gnu
   brew install coreutils
   brew install binutils
   brew install diffutils
-  brew install ed --with-default-names
   brew install findutils --with-default-names
   brew install gawk
   brew install gnu-indent --with-default-names
@@ -41,29 +57,84 @@ install_core() {
   brew install gnu-tar --with-default-names
   brew install gnu-which --with-default-names
   brew install gnutls
-  brew install grep --with-default-names
-  brew install gzip
-  brew install screen
-  brew install watch
+    brew install watch
   brew install wdiff --with-gettext
   brew install wget
   brew install bash
   brew install emacs
   brew install gdb  # gdb requires further actions to make it work. See `brew info gdb`.
-  brew install gpatch
-  brew install m4
-  brew install make
-  brew install nano
-  brew install file-formula
   brew install git
-  brew install less
-  brew install openssh
+
+  brew tap homebrew/versions
   brew install perl518   # must run "brew tap homebrew/versions" first!
   brew install python
-  brew install rsync
+  brew install grsync
   brew install svn
-  brew install unzip
   brew install vim --override-system-vi
-  brew install macvim --override-system-vim --custom-system-icons
   brew install zsh
+
+  # install virtualbox + vagrant
+  brew cask install virtualbox
+  brew cask install vagrant
+  brew cask install vagrant-bar
+  brew cask install vagrant-manager
+
+  brew cask install google-chrome
+
+  # install java + misc
+  brew cask install java
+  brew cask install hipchat
+  brew cask install caffeine
+  brew cask install skype
+  brew cask install keepassx
+  brew cask install sublime-text
+  brew cask install daisydisk
+  brew cask install packer
+  brew cask install viber
+  brew cask install kindle-previewer
+  brew cask install pokerstars
+  brew cask install limechat
+  brew cask install adobe-reader
+  brew cask install pycharm-ce
+  brew cask install filezilla
+  brew cask install evernote
+  brew cask install slack
+  brew cask install hubic
+  #brew install pass
+  #echo "source /usr/local/etc/bash_completion.d/password-store" >> ~/.bashrc
+  brew cask install dropbox
+  brew cask install disk-inventory-x
+
+
+  brew install ansible
+  gem install thor parseconfig
+
+  # install postgres + redis
+  brew install postgres
+  brew install python
+  brew install zsh
+  brew install nmap
+  brew install nodejs
+  brew install hub
+  brew install gh
+  brew install ack
+  brew install links
+  brew install tree
+
+
+  # install awscli
+  brew install pip
+  curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+  unzip awscli-bundle.zip
+  sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+  
+  pip install boto
+  pip install fabric
+
+  npm install gulp -gulp
+
+  # install docker
+  pip install -U docker-compose
+  brew cask install kitematic
+  brew cask install docker-machine
 }
